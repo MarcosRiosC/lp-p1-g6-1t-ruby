a03d11a375755c90c08c6a3c21c7145fa38bb1ee
aaron <aardreye@espol.edu.ec>
2021-07-13 20:06:51 -0500

    mas mejoras

--- Analizador_Sintactico.py
+++ Analizador_Sintactico.py
@@ -7,7 +7,6 @@ def p_sentencia(p):
     '''sentencia : funcion
                 | declaracion
                 | estructura
-                | print
                 '''
     p[0] = p [1]
 
@@ -37,7 +36,7 @@ variables_asignadas = []
 def p_declaracion_termino(p):
     'declaracion : variable EQUAL termino'
     p[0] = p[1]
-    variables_asignadas.append(p[0])
+    variables_asignadas.append(p[0] + "")
     print('variable asignada')
 
 def p_declaracion_string(p):
@@ -62,6 +61,12 @@ def p_declaracion_gets(p):
     'declaracion : variable EQUAL GETS'
     p[0] = str(p[1]) + p[2] + str(p[3])
 
+def p_declaracion_metodo(p):
+    'declaracion : variable EQUAL metodo'
+    p[0] = p[1]
+    variables_asignadas.append(p[0])
+    print('variable asignada')
+
 def p_variable(p):
     '''variable : VARIABLE_LOCAL
             | VARIABLE_INSTANCE
@@ -69,79 +74,6 @@ def p_variable(p):
             | VARIABLE_GLOBAL
             | CONSTANT'''
     p[0] = p[1]
-
-
-#REGLA SEMÁNTICA PARA OPERACIONES MATEMÁTICAS (INICIO)
-
-def p_operacion_plus(p):
-    'operacion : termino PLUS termino'
-    if (type(p[1]) == type(p[3]) == int):
-        p[0] = p[1] + p[3]
-        print('suma entre números')
-    else:
-        semantico.validar_variables(p, variables_asignadas, 'suma')
-
-def p_operacion_minus(p):
-    'operacion : termino MINUS termino'
-    if (type(p[1]) == type(p[3]) == int):
-        p[0] = p[1] - p[3]
-        print('resta entre números')
-    else:
-        semantico.validar_variables(p, variables_asignadas, 'resta')
-
-def p_operacion_multiplication(p):
-    'operacion : termino MULTIPLICATION termino'
-    if (type(p[1]) == type(p[3]) == int):
-        p[0] = p[1] * p[3]
-        print('multiplicación entre números')
-    else:
-        semantico.validar_variables(p, variables_asignadas, 'multiplicación')
-
-def p_operacion_divide(p):
-    'operacion : termino DIVIDE termino'
-    if (type(p[1]) == type(p[3]) == int):
-        p[0] = p[1] / p[3]
-        print('división entre números')
-    else:
-        semantico.validar_variables(p, variables_asignadas, 'división')
-
-def p_operacion_exponent(p):
-    'operacion : termino EXPONENT termino'
-    if (type(p[1]) == type(p[3]) == int):
-        p[0] = p[1] ** p[3]
-        print('potencia entre números')
-    else:
-        semantico.validar_variables(p, variables_asignadas, 'potencia')
-
-def p_operacion_module(p):
-    'operacion : termino MODULE termino'
-    if (type(p[1]) == type(p[3]) == int):
-        p[0] = p[1] % p[3]
-        print('módulo entre números')
-    else:
-        semantico.validar_variables(p, variables_asignadas, 'módulo')
-
-def p_termino_entero(p):
-    'termino : ENTERO'
-    p[0] = int(p[1])
-
-def p_termino_float(p):
-    'termino : FLOAT'
-    p[0] = float(p[1])
-
-def p_termino_var(p):
-    'termino : variable'
-    p[0] = p[1]
-
-def p_termino_expr(p):
-    'termino : L_PAREN operacion R_PAREN'
-    p[0] = p[2]
-
-def p_termino_operacion(p):
-    'termino : operacion'
-    p[0] = p[1]
-#REGLA SEMÁNTICA PARA OPERACIONES MATEMÁTICAS (FIN)
-
 
 def p_booleano_and_logic(p):
     'booleano : booleano AND_LOGIC booleano'
@@ -170,20 +102,6 @@ def p_booleano_smaller_than(p):
 def p_booleano_smaller_equal(p):
     'booleano : termino SMALLER_EQUAL termino'
     p[0] = p[1] <= p[3]
-
-
-#PRINT
-def p_print_operacion(p):
-    'print : PUTS L_PAREN operacion R_PAREN'
-
-def p_print_booleano(p):
-    'print : PUTS L_PAREN booleano R_PAREN'
-
-def p_print_var(p):
-    'print : PUTS L_PAREN variable R_PAREN'
-
-def p_print_vac(p):
-    'print : PUTS L_PAREN R_PAREN'
 
 
 #ESTRUCTURAS DE CONTROL
@@ -252,6 +170,109 @@ def p_arrayContext(p):
         | ENTERO COMMA arrayContext
     '''
 
+
+#PRINT
+def p_puts_operacion(p):
+    'puts : PUTS L_PAREN operacion R_PAREN'
+
+def p_puts_booleano(p):
+    'puts : PUTS L_PAREN booleano R_PAREN'
+
+def p_puts_var(p):
+    'puts : PUTS L_PAREN variable R_PAREN'
+
+def p_puts_vac(p):
+    'puts : PUTS L_PAREN R_PAREN'
+
+
+#Metodos
+def p_metodo_class(p):
+    'metodo : termino POINT CLASS'
+
+def p_metodo_to_i(p):
+    'metodo : termino POINT TO_I'
+
+def p_metodo_to_f(p):
+    'metodo : termino POINT TO_F'
+
+def p_metodo_to_s(p):
+    'metodo : termino POINT TO_S'
+
+
+#REGLA SEMÁNTICA PARA OPERACIONES MATEMÁTICAS (INICIO)
+
+def p_operacion_plus(p):
+    'operacion : termino PLUS termino'
+    if (type(p[1]) == type(p[3]) == int):
+        p[0] = p[1] + p[3]
+        print('suma entre números')
+    else:
+        semantico.validar_variables(p, variables_asignadas, 'suma')
+
+def p_operacion_minus(p):
+    'operacion : termino MINUS termino'
+    if (type(p[1]) == type(p[3]) == int):
+        p[0] = p[1] - p[3]
+        print('resta entre números')
+    else:
+        semantico.validar_variables(p, variables_asignadas, 'resta')
+
+def p_operacion_multiplication(p):
+    'operacion : termino MULTIPLICATION termino'
+    if (type(p[1]) == type(p[3]) == int):
+        p[0] = p[1] * p[3]
+        print('multiplicación entre números')
+    else:
+        semantico.validar_variables(p, variables_asignadas, 'multiplicación')
+
+def p_operacion_divide(p):
+    'operacion : termino DIVIDE termino'
+    if (type(p[1]) == type(p[3]) == int):
+        p[0] = p[1] / p[3]
+        print('división entre números')
+    else:
+        semantico.validar_variables(p, variables_asignadas, 'división')
+
+def p_operacion_exponent(p):
+    'operacion : termino EXPONENT termino'
+    if (type(p[1]) == type(p[3]) == int):
+        p[0] = p[1] ** p[3]
+        print('potencia entre números')
+    else:
+        semantico.validar_variables(p, variables_asignadas, 'potencia')
+
+def p_operacion_module(p):
+    'operacion : termino MODULE termino'
+    if (type(p[1]) == type(p[3]) == int):
+        p[0] = p[1] % p[3]
+        print('módulo entre números')
+    else:
+        semantico.validar_variables(p, variables_asignadas, 'módulo')
+
+def p_termino_entero(p):
+    'termino : ENTERO'
+    p[0] = int(p[1])
+
+def p_termino_float(p):
+    'termino : FLOAT'
+    p[0] = float(p[1])
+
+def p_termino_var(p):
+    'termino : variable'
+    p[0] = p[1]
+
+def p_termino_expr(p):
+    'termino : L_PAREN operacion R_PAREN'
+    p[0] = p[2]
+
+def p_termino_string(p):
+    'termino : STRING'
+    p[0] = p[1]
+
+def p_termino_operacion(p):
+    'termino : operacion'
+    p[0] = p[1]
+#REGLA SEMÁNTICA PARA OPERACIONES MATEMÁTICAS (FIN)
 
 #DE AQUÍ EN ADELANTE EMPIEZA EL COPY PASTE SALVAJE (CÓDIGO RECICLADO DE LA PRÁCTICA EN CLASES)
 #EVALUADOR DE ERRORES
